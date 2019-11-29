422fb8a639f981dd3667872a616a285f6958e0b6
NuthanChandra <nuthanchandra1997@gmail.com>
2019-11-29 20:52:56 +0530

    Context object name for custom context dictionary

--- basic_app/views.py
+++ basic_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View,TemplateView,ListView,DetailView
 from . import models
 # Create your views here.
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
 
 class SchoolDetailedView(DetailView):
