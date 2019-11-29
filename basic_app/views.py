9cd425a31f254898fd73594d6f95060cb386f6e3
NuthanChandra <nuthanchandra1997@gmail.com>
2019-11-29 20:55:11 +0530

    Update school list and not school detail

--- basic_app/views.py
+++ basic_app/views.py
@@ -7,5 +7,6 @@ class SchoolListView(ListView):
     model = models.School
 
 class SchoolDetailedView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
